dae2eb28be82a08edc94e995cd15a1ac9c725cdd
Luis Henrique <euluishenrique7@gmail.com>
2023-10-24 11:01:37 -0300

    *

---
+++ src/app/Portal/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../authentication/authentication.service';
+import jwt_decode from 'jwt-decode';
+import { TokenService } from 'src/app/authentication/token.service';
+import { User } from 'src/app/authentication/user/user';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  email = '';
+  senha = '';
+
+  constructor(private authservice: AuthService, private router: Router,  private tokenService: TokenService) {}
+
+  ngOnInit(): void {
+  }
+
+  doLogin(): any{
+    this.authservice.doLogin(this.email, this.senha).subscribe({
+      next: (res)=> {
+        const user = jwt_decode(res.body.token) as User;
+        this.tokenService.saveToken(res.body.token);
+        this.tokenService.returnToken();
+
+        if(user["nivel_acesso"] === 'Solicitante'){
+
+          return this.router.navigate(['/solicitar']);
+        }
+        if(user["nivel_acesso"] === 'Administrador'){
+
+          return this.router.navigate(['/home']);
+        }
+        if(user["nivel_acesso"] === 'Visualizador'){
+          return this.router.navigate(['/eventos-presenca']);
+        }
+         return this.router.navigate(['/home']);
+      },
+      error:(error)=>{
+        alert('Email ou senha inválido.');
+        console.log(error);
+      },
+    });
+  }
+
+
+  irParaTipoCadastro() {
+    this.router.navigate(['/type-register']);
+  }
+
+}
---
+++ src/app/authentication/user/user.ts
@@ -0,0 +1,8 @@
+export interface User { 
+    id?: number,
+    email?: string,
+    senha?: string,
+    telefone?: number,
+    nivel_acesso?: string,
+    id_instituicao?: number
+}
