80d6db75e7c40826cb43ef09d30fe38a797c5b8c
Daniel Vieira <126349675+DNLVRDCNH@users.noreply.github.com>
2023-11-21 07:53:07 -0300

    alterar evento

--- src/app/Eventos/alterar-evento/alterar-evento.component.ts
+++ src/app/Eventos/alterar-evento/alterar-evento.component.ts
@@ -14,6 +14,7 @@ import { Instituicoes } from './instituicao';
   styleUrls: ['./alterar-evento.component.scss', '../styles.scss']
 })
 export class AlterarEventoComponent implements OnInit {
+[x: string]: any;
 
   form: FormGroup;
   submitted = false;
